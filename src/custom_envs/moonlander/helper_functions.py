701911df54b1c159d93404b63895db02ff94cc52
Carolin Klute <carolin.klute@uni-bielefeld.de>
2025-03-31 12:50:10 +0200

    more meta env testing

--- src/custom_envs/moonlander/helper_functions.py
+++ src/custom_envs/moonlander/helper_functions.py
@@ -3,12 +3,8 @@ from typing import List, Dict, Tuple
 import copy
 import math
 import cv2
-import yaml
-import csv
 import numpy as np
 import scipy.stats
-
-from src import ROOT_DIR
 
 
 def create_ranges_of_objects_funnels_and_drifts(
@@ -936,27 +932,3 @@ def calculate_gaussian_reward(state, collected_objects: list[dict], agent_size: 
     else:
         normalized_reward = normalized_reward - len(collected_objects) * 500
     return int(normalized_reward), object_dict_list
-
-def read_test_data(name):
-    filename = name + ".csv"
-    with open(ROOT_DIR / "test_data" / filename, "r") as file:
-        lines = csv.reader(file)
-        test_data = []
-        current_observation = []
-        for line in lines:
-            if len(line) > 0:
-                string_row = list(line)
-                row = list(map(float, string_row))
-                current_observation.append(row)
-            else:
-                test_data.append(current_observation)
-                current_observation = []
-
-        return test_data
-
-
-def load_test_config(name):
-    filename = name + ".yaml"
-    with open(ROOT_DIR / "test_data" / "levels" / filename, "r") as file:
-        return yaml.safe_load(file)
-
