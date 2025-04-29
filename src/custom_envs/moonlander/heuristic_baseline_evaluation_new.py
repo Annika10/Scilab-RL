0da7ed178c2bd26f015eaed75237e85ddf7a4285
annika <annika.oesterdiekhoff@uni-bielefeld.de>
2025-04-29 15:00:15 +0200

    add switching in every frame

--- src/custom_envs/moonlander/heuristic_baseline_evaluation_new.py
+++ src/custom_envs/moonlander/heuristic_baseline_evaluation_new.py
@@ -125,6 +125,11 @@ def evaluate_policy(
         ### added by me
         if action_sequence:
             actions = np.array([action_sequence[counter]])
+        elif name_of_heuristic == "switch_every_frame":
+            if last_action == np.array([0]):
+                actions = np.array([1])
+            elif last_action == np.array([1]):
+                actions = np.array([0])
         else:
             if observations[0][0] > observations[0][1]:
                 actions = np.array([1])
@@ -336,14 +341,12 @@ def calculate_action_sequence_for_switch_per_percentage(dodge_percentage: float,
 if __name__ == "__main__":
     ### DEFINE BEFORE ###
     
-    mode = "switch_per_percentage"  # "SoC_switching", "switch_per_percentage"
+    mode = "switch_every_frame"  # "SoC_switching", "switch_per_percentage", "switch_every_frame"
     percentage_pairs = [[0, 1], [0.05, 0.95], [0.1, 0.9], [0.15, 0.85], [0.2, 0.8], [0.25, 0.75], [0.3, 0.7],
                         [0.35, 0.65], [0.4, 0.6], [0.45, 0.55], [0.5, 0.5], [0.55, 0.45], [0.6, 0.4], [0.65, 0.35],
                         [0.7, 0.3], [0.75, 0.25], [0.8, 0.2], [0.85, 0.15], [0.9, 0.1], [0.95, 0.05], [1, 0]]
     task_one_difficulty = "easy"
     task_two_difficulty = "hard"
-    filename_collect_task_one = f"collect_{task_one_difficulty}_object_list_30_times_40_0.csv"
-    filename_collect_task_two = f"collect_{task_two_difficulty}_object_list_30_times_40_1.csv"
     
     render = False
     n_eval_episodes = 100
@@ -351,6 +354,9 @@ if __name__ == "__main__":
     ####################
     
     meta_env_name = f"MetaEnv-pretrained-without-SoC-{task_one_difficulty}-{task_two_difficulty}-v0"
+    
+    filename_collect_task_one = f"collect_{task_one_difficulty}_object_list_30_times_40_0.csv"
+    filename_collect_task_two = f"collect_{task_two_difficulty}_object_list_30_times_40_1.csv"
     
     if task_one_difficulty == "easy":
         collect_task_one_best_model_name = "collect_gaussian_with_distance_easy_positions_07_04_2025_best_model"
@@ -371,8 +377,10 @@ if __name__ == "__main__":
     
     if mode == "SoC_switching":
         filepath_for_storage = directory / f"{collect_task_one_best_model_name}_{collect_task_two_best_model_name}_soc_switching.csv"
-    else:
+    elif mode == "switch_per_percentage":
         filepath_for_storage = directory / f"{collect_task_one_best_model_name}_{collect_task_two_best_model_name}_percentage_pairs.csv"
+    else:
+        filepath_for_storage = directory / f"{collect_task_one_best_model_name}_{collect_task_two_best_model_name}_switch_every_frame.csv"
     
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -442,3 +450,24 @@ if __name__ == "__main__":
             return_episode_rewards=False,
             filepath_for_storage=filepath_for_storage,
             name_of_heuristic="SoC switching")
+    
+    elif mode == "switch_every_frame":
+        # Initialise the environment
+        env = gym.make(meta_env_name, render_mode="human",
+                       collect_task_one_best_model_name=collect_task_one_best_model_name,
+                       collect_task_two_best_model_name=collect_task_two_best_model_name,
+                       config_file_name_collect_task_one=config_file_name_collect_task_one,
+                       config_file_name_collect_task_two=config_file_name_collect_task_two,
+                       list_of_object_dict_lists_collect_task_one_filename=filename_collect_task_one,
+                       list_of_object_dict_lists_collect_task_two_filename=filename_collect_task_two,
+                       input_noise_in_subtasks_on=False, consecutive_frames=5)
+        
+        _, _ = evaluate_policy(
+            env=env,
+            # I don't know why, but it is the same in the standard evaluate_policy used by the framework
+            n_eval_episodes=n_eval_episodes / 2,
+            deterministic=True,
+            render=render,
+            return_episode_rewards=False,
+            filepath_for_storage=filepath_for_storage,
+            name_of_heuristic="switch_every_frame")
