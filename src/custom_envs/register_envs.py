3989916b452c4edd229bf6d8941d22eeaaa0ce6d
annika <annika.oesterdiekhoff@uni-bielefeld.de>
2025-05-06 15:59:41 +0200

    fix wrong trained model loading

--- src/custom_envs/register_envs.py
+++ src/custom_envs/register_envs.py
@@ -488,7 +488,7 @@ def register_custom_envs():
              entry_point="src.custom_envs.moonlander.meta_env_pretrained_without_soc:MetaEnvPretrainedWithoutSoC",
              kwargs={
                  'collect_task_one_best_model_name': "collect_gaussian_with_distance_easy_input_noise_positions_07_04_2025_best_model",
-                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_easy_input_noise_positions_07_04_2025_best_model",
+                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_easy_positions_07_04_2025_best_model",
                  'config_file_name_collect_task_one': "config_collect_easy.yaml",
                  'config_file_name_collect_task_two': "config_collect_easy.yaml",
                  'consecutive_frames': 5,
@@ -533,8 +533,8 @@ def register_custom_envs():
     register(id="MetaEnv-pretrained-without-SoC-easy-hard-input-noise-in-hard-task-v0",
              entry_point="src.custom_envs.moonlander.meta_env_pretrained_without_soc:MetaEnvPretrainedWithoutSoC",
              kwargs={
-                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_easy_input_noise_positions_07_04_2025_best_model",
-                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
+                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_easy_positions_07_04_2025_best_model",
+                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_input_noise_positions_07_04_2025_best_model",
                  'config_file_name_collect_task_one': "config_collect_easy.yaml",
                  'config_file_name_collect_task_two': "config_collect_hard.yaml",
                  'consecutive_frames': 5,
@@ -546,7 +546,7 @@ def register_custom_envs():
              entry_point="src.custom_envs.moonlander.meta_env_pretrained_without_soc:MetaEnvPretrainedWithoutSoC",
              kwargs={
                  'collect_task_one_best_model_name': "collect_gaussian_with_distance_easy_input_noise_positions_07_04_2025_best_model",
-                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
+                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_input_noise_positions_07_04_2025_best_model",
                  'config_file_name_collect_task_one': "config_collect_easy.yaml",
                  'config_file_name_collect_task_two': "config_collect_hard.yaml",
                  'consecutive_frames': 5,
@@ -567,7 +567,7 @@ def register_custom_envs():
     register(id="MetaEnv-pretrained-without-SoC-hard-hard-input-noise-in-one-task-v0",
              entry_point="src.custom_envs.moonlander.meta_env_pretrained_without_soc:MetaEnvPretrainedWithoutSoC",
              kwargs={
-                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
+                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_hard_positions_input_noise_07_04_2025_best_model",
                  'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
                  'config_file_name_collect_task_one': "config_collect_hard.yaml",
                  'config_file_name_collect_task_two': "config_collect_hard.yaml",
@@ -579,8 +579,8 @@ def register_custom_envs():
     register(id="MetaEnv-pretrained-without-SoC-hard-hard-input-noise-v0",
              entry_point="src.custom_envs.moonlander.meta_env_pretrained_without_soc:MetaEnvPretrainedWithoutSoC",
              kwargs={
-                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
-                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_positions_31_03_2025_best_model",
+                 'collect_task_one_best_model_name': "collect_gaussian_with_distance_hard_positions_input_noise_07_04_2025_best_model",
+                 'collect_task_two_best_model_name': "collect_gaussian_with_distance_hard_positions_input_noise_07_04_2025_best_model",
                  'config_file_name_collect_task_one': "config_collect_hard.yaml",
                  'config_file_name_collect_task_two': "config_collect_hard.yaml",
                  'consecutive_frames': 5,
